fb0c441ace06ae6e1a26a480f176c6156b60ed60
Adrian Dobre <50677374+adrian-dobre@users.noreply.github.com>
2022-12-09 12:22:46 +0200

    Initial commit

---
+++ LivoloRemote.cpp
@@ -0,0 +1,69 @@
+
+#include "LivoloRemote.h"
+
+LivoloRemote::LivoloRemote(unsigned int pin) {
+    pinMode(pin, OUTPUT);
+    LivoloRemote::pin = pin;
+}
+
+void LivoloRemote::send(unsigned int remoteId, unsigned int keyCode,
+                        unsigned int repeats) {
+    LivoloRemote::encodedSequenceLength = 0;
+    LivoloRemote::output = HIGH;
+    LivoloRemote::encode(1, true);
+
+    for (int i = 15; i >= 0; i--) {
+        LivoloRemote::encode(bitRead(remoteId, i));
+    }
+
+    for (int i = 6; i >= 0; i--) {
+        LivoloRemote::encode(bitRead(keyCode, i));
+    }
+
+    for (int i = 0; i <= repeats; i++) {
+        LivoloRemote::transmit();
+    }
+}
+void LivoloRemote::toggleOutput() {
+    if (LivoloRemote::output == HIGH) {
+        LivoloRemote::output = LOW;
+    } else {
+        LivoloRemote::output = HIGH;
+    }
+}
+
+void LivoloRemote::encode(unsigned int bit, bool preamble) {
+    int pulseLength = shortLength;
+    int repeats = 0;
+    if (preamble) {
+        pulseLength = preambleLength;
+    } else if (bit == 1) {
+        pulseLength = longLength;
+    } else {
+        repeats = 1;
+    }
+    for (int i = 0; i <= repeats; i++) {
+        LivoloRemote::appentToSequence(LivoloRemote::output, pulseLength);
+        LivoloRemote::toggleOutput();
+    }
+}
+
+void LivoloRemote::appentToSequence(unsigned int bit,
+                                    unsigned int pulseLength) {
+    LivoloRemote::encodedSequence[LivoloRemote::encodedSequenceLength] = bit;
+    LivoloRemote::encodedSequence[LivoloRemote::encodedSequenceLength + 1] =
+        pulseLength;
+    LivoloRemote::encodedSequenceLength += 2;
+}
+
+void LivoloRemote::transmit() {
+    unsigned int output = HIGH;
+    for (int i = 0; i < LivoloRemote::encodedSequenceLength; i += 2) {
+        output = LivoloRemote::encodedSequence[i];
+        digitalWrite(LivoloRemote::pin, output);
+        delayMicroseconds(LivoloRemote::encodedSequence[i + 1]);
+    }
+    if (output == HIGH) {
+        digitalWrite(LivoloRemote::pin, LOW);
+    }
+}
---
+++ LivoloRemote.h
@@ -0,0 +1,64 @@
+/**
+ * @file LivoloRemote.h
+ * @author Adrian Dobre (adi.dobre90@gmail.com)
+ * @brief Remote Library (TX only) for Livolo RF switches.
+ * Thanks kranjec.primoz@gmail.com for the research done on his blog
+ * https://neofoxx.com/post-4.html, kind3r@gmail.com for the keycodes
+ * (https://github.com/kind3r/livolo-rf-codes) and to all the people that
+ * gathered various info here
+ * https://community.home-assistant.io/t/broadlink-rm-pro-livolo-switches-rf-learning-woes/12432
+ * and here
+ * https://forum.arduino.cc/t/control-livolo-switches-livolo-switch-library/149850
+ * @version 0.1
+ * @date 2022-12-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#ifndef LivoloRemote_h
+#define LivoloRemote_h
+
+#include "Arduino.h"
+
+class LivoloRemote {
+   public:
+    LivoloRemote(unsigned int pin);
+    void send(unsigned int remoteId, unsigned int keyCode,
+              unsigned int repeats = 200);
+
+   private:
+    unsigned int pin;
+    unsigned int output = HIGH;
+    unsigned int shortLength = 162;
+    unsigned int preambleLength = 530;
+    unsigned int longLength = 320;
+    unsigned int encodedSequence[100];
+    unsigned int encodedSequenceLength = 0;
+    void encode(unsigned int bit, bool preamble = false);
+    void appentToSequence(unsigned int bit, unsigned int pulseLength);
+    void transmit();
+    void toggleOutput();
+};
+
+namespace LivoloRemoteKey {
+const int BUTTON_1 = 0;
+const int BUTTON_2 = 96;
+const int BUTTON_3 = 120;
+const int BUTTON_4 = 24;
+const int BUTTON_5 = 108;
+const int BUTTON_6 = 80;
+const int BUTTON_7 = 48;
+const int BUTTON_8 = 12;
+const int BUTTON_9 = 72;
+const int BUTTON_10 = 40;
+const int SCENE_1 = 90;
+const int SCENE_2 = 114;
+const int SCENE_3 = 10;
+const int SCENE_4 = 18;
+const int ALL_OFF = 106;
+const int DIMMER_TOGGLE = 8;
+const int DIMMER_UP = 16;
+const int DIMMER_DOWN = 56;
+}  // namespace LivoloRemoteKey
+
+#endif
